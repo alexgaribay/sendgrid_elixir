b6992f6c32ac18d46b6c8fd93fcbb557bde8068f
Alex Garibay <alex@alexgaribay.com>
2016-05-22 13:23:07 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SendGrid.Mixfile do
 
   def project do
     [app: :sendgrid,
-     version: "0.0.2",
+     version: "0.1.0",
      elixir: "~> 1.2",
      package: package,
      description: description,
